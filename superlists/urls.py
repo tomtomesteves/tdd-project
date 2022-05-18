c1fd66982853a8b61428631f5da0f8e282ff8c6e
Thomas Esteves <thomas.esteves@bancointer.com.br>
2022-05-18 20:53:22 -0300

    Implementando o Novo Projeto: Ajustando o Modelo

--- superlists/urls.py
+++ superlists/urls.py
@@ -13,12 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import re_path
+from django.conf.urls import url
 from lists import views
 
 
 urlpatterns = [
-    re_path(r'^$', views.home_page, name='home'),
-    re_path(r'^lists/new$', views.new_list, name='new_list'),
-    re_path(r'^lists/the-only-list-in-the-world/$', views.view_list, name='view_list'),
+    url(r'^$', views.home_page, name='home'),
+    url(r'^lists/new$', views.new_list, name='new_list'),
+    url(r'^lists/the-only-list-in-the-world/$', views.view_list, name='view_list'),
 ]
