f0fb71caea94c795c9397be2987ea0b895c9e241
Thomas Esteves <thomas.esteves@bancointer.com.br>
2022-06-17 20:49:22 -0300

    Refatorando o Teste Funcional

---
+++ functional_tests/test_simple_list_creation.py
@@ -0,0 +1,40 @@
+import unittest
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
+from functional_tests.base import FunctionalTest
+import time
+
+MAX_WAIT = 5
+
+
+class LayoutAndStylingTest(FunctionalTest):
+        # Satisfeitos, ambos vão dormir
+    def test_layout_and_styling(self):
+        # Edith entra na home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # Ela nota que o input box está centralizado
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+
+        # Ela inicia uma nova lista e nota que o input
+        # também está centralizado
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+
+if __name__ == '__main__':
+    unittest.main()
